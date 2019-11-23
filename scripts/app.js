d8f97f974311c9ca79af1e738edd6604ac18c556
crissesoto <soto.crisse@hotmail.com>
2019-11-23 21:57:53 +0100

    updated UI through dom app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,40 @@
 const form = document.querySelector("form");
+const card = document.querySelector(".card");
+const weatherDetails = document.querySelector(".details");
+
+const updateUI = object => {
+  const cityDetails = object.cityKey;
+  const weather = object.currentWeather;
+
+  console.log(cityDetails, weather);
+
+  // remove d-none class if it's present
+
+  if (card.classList.contains("d-none")) {
+    card.classList.remove("d-none");
+  }
+  // update details div template
+
+  weatherDetails.innerHTML = `
+  <h5 class="my-3">${cityDetails.EnglishName}</h5>
+  <div class="my-3">${weather.WeatherText}</div>
+  <div class="display-4 my-4">
+    <span>${Math.floor(weather.Temperature.Metric.Value)}</span>
+    <span>&deg;C</span>
+  </div>
+  `;
+};
+
+const updateCity = async city => {
+  const cityKey = await getCity(city);
+
+  const currentWeather = await getCurrentWeather(cityKey.Key);
+
+  return {
+    cityKey: cityKey,
+    currentWeather: currentWeather
+  };
+};
 
 form.addEventListener("submit", e => {
   e.preventDefault();
@@ -8,5 +44,7 @@ form.addEventListener("submit", e => {
   form.reset();
 
   // update UI
-  updateUI();
+  updateCity(searchedCity)
+    .then(object => updateUI(object))
+    .catch(error => console.log("rejected:", error));
 });
--- scripts/forecast.js
+++ scripts/forecast.js
@@ -8,7 +8,6 @@ const getCurrentWeather = async cityKey => {
   const data = await response.json();
 
   return data[0];
-  //console.log(data[0]);
 };
 
 const getCity = async cityname => {
@@ -19,18 +18,5 @@ const getCity = async cityname => {
   const response = await fetch(baseURL + query);
   const data = await response.json();
 
-  return data[0].Key;
-
-  //console.log(data[0].Key);
+  return data[0];
 };
-
-getCity("bruges")
-  .then(cityKey => {
-    return getCurrentWeather(cityKey);
-  })
-  .then(currentWeather => {
-    console.log(currentWeather);
-  })
-  .catch(error => {
-    console.log("rejected:", error);
-  });
