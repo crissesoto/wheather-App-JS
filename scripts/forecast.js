842ee6a8be6c883f5ee8d5e081cc98d5c846af38
crissesoto <soto.crisse@hotmail.com>
2019-11-23 19:52:39 +0100

    added forecast.js code

---
+++ scripts/forecast.js
@@ -0,0 +1,36 @@
+const key = "hF5ZltzAF206HAFuqG2kmIMkrAHXUuds";
+
+const getCurrentWeather = async cityKey => {
+  const baseURL = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${cityKey}?apikey=${key}`;
+
+  const response = await fetch(baseURL + query);
+  const data = await response.json();
+
+  return data[0];
+  //console.log(data[0]);
+};
+
+const getCity = async cityname => {
+  const baseURL =
+    "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${cityname}`;
+
+  const response = await fetch(baseURL + query);
+  const data = await response.json();
+
+  return data[0].Key;
+
+  //console.log(data[0].Key);
+};
+
+getCity("london")
+  .then(cityKey => {
+    return getCurrentWeather(cityKey);
+  })
+  .then(currentWeather => {
+    console.log(currentWeather);
+  })
+  .catch(error => {
+    console.log("rejected:", error);
+  });
