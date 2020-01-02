26bfb4144f24174bac1596cbf45160160b596755
crissesoto <soto.crisse@hotmail.com>
2020-01-02 16:44:51 +0100

    added comments to forecast.js

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,6 +1,7 @@
 // https://developer.accuweather.com/
 const key = "nrOGV86jnhxLczGGEhNruxAGVr6rcdGA";
 
+// get weather info
 const getCurrentWeather = async cityKey => {
   const baseURL = "http://dataservice.accuweather.com/currentconditions/v1/";
   const query = `${cityKey}?apikey=${key}`;
@@ -11,6 +12,7 @@ const getCurrentWeather = async cityKey => {
   return data[0];
 };
 
+// get city info
 const getCity = async cityname => {
   const baseURL =
     "http://dataservice.accuweather.com/locations/v1/cities/search";
